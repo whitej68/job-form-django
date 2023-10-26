c63ef514624b38dc9561bcbd5e3447df560708dc
whitej68 <114672020+whitej68@users.noreply.github.com>
2023-10-26 18:03:51 -0400

    Added navigation bar with about and contact links

--- job_application/views.py
+++ job_application/views.py
@@ -25,3 +25,9 @@ def index(request):
 
     return render(request, "index.html")
 
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
